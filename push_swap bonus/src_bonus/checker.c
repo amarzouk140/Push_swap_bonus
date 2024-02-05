4cbdba874795d0ecf5432612317debd70b19c053
Ayman <156066641+amarzouk140@users.noreply.github.com>
2024-02-05 19:13:39 +0400

    Update checker.c
    update strcmp instead of strncmp

--- push_swap bonus/src_bonus/checker.c
+++ push_swap bonus/src_bonus/checker.c
@@ -24,34 +24,33 @@ void	free_checker(t_stack_node **a, t_stack_node **b, char **line)
 
 int	do_commands(char *line, t_stack_node **stack_a, t_stack_node **stack_b)
 {
-	if (!(ft_strncmp(line, "sa", 2)))
-		return (swap(stack_a), 0);
-	else if (!(ft_strncmp(line, "sb", 2)))
-		return (swap(stack_b), 0);
-	else if (!(ft_strncmp(line, "ss", 2)))
+	if (!(ft_strcmp(line, "sa\n")))
+		return (sa(stack_a, 1), 0);
+	if (!(ft_strcmp(line, "sb\n")))
+		return (sb(stack_b, 1), 0);
+	if (!(ft_strcmp(line, "ss\n")))
 		return (ss(stack_a, stack_b, 1), 0);
-	else if (!(ft_strncmp(line, "pa", 2)))
-		return (push(stack_a, stack_b), 0);
-	else if (!(ft_strncmp(line, "pb", 2)))
-		return (push(stack_b, stack_a), 0);
-	else if (!(ft_strncmp(line, "ra", 2)))
-		return (rotate(stack_a), 0);
-	else if (!(ft_strncmp(line, "rb", 2)))
-		return (rotate(stack_b), 0);
-	else if (!(ft_strncmp(line, "rr", 2)))
+	if (!(ft_strcmp(line, "pa\n")))
+		return (pa(stack_a, stack_b, 1), 0);
+	if (!(ft_strcmp(line, "pb\n")))
+		return (pb(stack_b, stack_a, 1), 0);
+	if (!(ft_strcmp(line, "ra\n")))
+		return (ra(stack_a, 1), 0);
+	if (!(ft_strcmp(line, "rb\n")))
+		return (rb(stack_b, 1), 0);
+	if (!(ft_strcmp(line, "rr\n")))
 		return (rr(stack_a, stack_b, 1), 0);
-	else if (!(ft_strncmp(line, "rra", 3)))
-		return (rev_rotate(stack_a), 0);
-	else if (!(ft_strncmp(line, "rrb", 3)))
-		return (rev_rotate(stack_b), 0);
-	else if (!(ft_strncmp(line, "rrr", 3)))
+	if (!(ft_strcmp(line, "rra\n")))
+		return (rra(stack_a, 1), 0);
+	if (!(ft_strcmp(line, "rrb\n")))
+		return (rrb(stack_b, 1), 0);
+	if (!(ft_strcmp(line, "rrr\n")))
 		return (rrr(stack_a, stack_b, 1), 0);
-	else
-		free_checker(stack_a, stack_b, &line);
-	return (1);
+	return (free_checker(stack_a, stack_b, &line), 1);
 }
 
-void	print_checker_res(t_stack_node **stack_a, t_stack_node **stack_b)
+void	print_checker_res(t_stack_node **stack_a, t_stack_node **stack_b,
+		char **line)
 {
 	if (stack_sorted(*stack_a))
 		ft_printf("OK\n");
@@ -61,6 +60,8 @@ void	print_checker_res(t_stack_node **stack_a, t_stack_node **stack_b)
 		free_stack(stack_a);
 	if (*stack_b)
 		free_stack(stack_b);
+	if (*line)
+		free(*line);
 }
 
 int	main(int ac, char **av)
@@ -81,11 +82,9 @@ int	main(int ac, char **av)
 	line = get_next_line(0);
 	while (line)
 	{
-		if (do_commands(line, &a, &b))
-			free_checker(&a, &b, &line);
+		do_commands(line, &a, &b);
 		free(line);
 		line = get_next_line(0);
 	}
-	print_checker_res(&a, &b);
-	return (0);
+	print_checker_res(&a, &b, &line);
 }
